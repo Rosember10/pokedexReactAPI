e0c7d072f76c6d016988c8830fbfda493ba79e6e
Rosember10 <rosember.01@outlook.com>
2023-03-18 01:09:57 -0400

    modification accueil.js

--- src/composant/Accueil/Accueil.js
+++ src/composant/Accueil/Accueil.js
@@ -1,7 +1,5 @@
 
 import "./Accueil.css";
-
-import audio from "./audio/theme.mp3"
 import React from "react";
 
 export default function Accueil() {
@@ -12,18 +10,7 @@ export default function Accueil() {
 
     return (
         <div>
-
-            {/* <video src={introVideo2} preload="auto" loop="" muted="" autoplay="" playsinline="" width="600" height="400"  /> */}
-            {/* <video autoPlay loop muted width="600" height="400" >
-                <source src={introVideo2} />
-            </video> */}
-
-            {/* <audio controls autoplay /> */}
-
-            <source src={audio} type="audio/mpeg" />
-
-
-
+            {/* <source src={audio} type="audio/mpeg" /> */}
         </div>
     );
 }
